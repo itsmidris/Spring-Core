390011e2158176a319cf3619ad18c8156e7f8c0c
itsmidris <idrishiimran72@gmail.com>
2026-06-22 16:14:07 +0530

    Service, Component and Autowired annotation

---
+++ src/test/java/com/test/BootP01/BootP01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.BootP01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootP01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
