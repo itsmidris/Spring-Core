1b2ac0a36c185d505a098e30a3ba7b433f843cd0
itsmidris <idrishiimran72@gmail.com>
2026-06-22 22:49:14 +0530

    Glimpse of Spring Boot

---
+++ BootP02/src/test/java/com/test/BootP02/BootP02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.BootP02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootP02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
