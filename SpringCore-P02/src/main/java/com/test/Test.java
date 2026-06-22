632d77daf532719a50a49fb9d56a52de3eefcab7
itsmidris <idrishiimran72@gmail.com>
2026-06-22 11:41:52 +0530

    Singleton and Prototype Bean scope

--- SpringCore-P02/src/main/java/com/test/Test.java
+++ SpringCore-P02/src/main/java/com/test/Test.java
@@ -12,8 +12,19 @@ public class Test {
         System.out.println("-----------Bean file Loaded---------------");
 
         System.out.println("Sending Request..........");
-        Car car = context.getBean(Car.class);
 
-        car.drive();
+        System.out.println("-------First Call--------");
+        Car car1 = context.getBean(Car.class);
+        System.out.println("-------Second Call--------");
+        Car car2 = context.getBean(Car.class);
+        System.out.println("Same object? " + (car1==car2));
+        System.out.println("By default Singleton Scope Or WE CAN SET");
+
+        System.out.println("-------First Call--------");
+        PetrolEngine p1 = context.getBean(PetrolEngine.class);
+        System.out.println("-------Second Call--------");
+        PetrolEngine p2 = context.getBean(PetrolEngine.class);
+        System.out.println("Same petrol Engine object? " + (p1==p2));
+        System.out.println("We set the scope of petrol Engine Object");
     }
 }
