e35d06900c1436d7b66ccd1e78ed2c50e1fda20e
itsmidris <idrishiimran72@gmail.com>
2026-06-22 16:43:55 +0530

    Service, Component & Autowired

---
+++ BootP01/src/main/java/com/test/BootP01/BootP01Application.java
@@ -0,0 +1,13 @@
+package com.test.BootP01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootP01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootP01Application.class, args);
+	}
+
+}
