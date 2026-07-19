0ca09fda19eab937ff17077aa95b07a444ebaf2d
itsmidris <idrishiimran72@gmail.com>
2026-07-19 17:14:30 +0530

    Thymeleaf selection expression

--- Thymleaf-Tutorial/src/main/java/com/imran/Thymleaf_Tutorial/controller/UserController.java
+++ Thymleaf-Tutorial/src/main/java/com/imran/Thymleaf_Tutorial/controller/UserController.java
@@ -2,6 +2,7 @@ package com.imran.Thymleaf_Tutorial.controller;
 
 
 import com.imran.Thymleaf_Tutorial.controller.model.User;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,5 +17,14 @@ public class UserController {
         User user = new User("Ramesh", "ramesh@gmail.com","ADMIN", "Male" );
         model.addAttribute("user", user);
         return "variable-expression";
+    }
+
+    //handler method to handle selection expression.
+    @GetMapping("selection-expression")
+    public String selectionExpression(Model model) {
+        User user = new User("Imran", "Imran@gmail.com", "ADMIN", "MALE");
+        model.addAttribute("user", user);
+
+        return "selection-expression";
     }
 }
